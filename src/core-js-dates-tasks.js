5ca58025ad565d4b8ee34b7756f87f58129d676b
AleksPanda <149190602+AleksPanda@users.noreply.github.com>
2026-01-05 20:33:10 +0200

    feat: 8 passing / 6 pending

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -108,8 +108,8 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
@@ -123,8 +123,16 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const startDate = new Date(dateStart);
+  const endDate = new Date(dateEnd);
+
+  const timeDifference = endDate.getTime() - startDate.getTime();
+
+  const millisecondsPerDay = 24 * 60 * 60 * 1000;
+  const dayDifference = timeDifference / millisecondsPerDay;
+
+  return Math.floor(dayDifference) + 1;
 }
 
 /**
@@ -144,8 +152,12 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const dateTimestamp = new Date(date).getTime();
+  const startTimestamp = new Date(period.start).getTime();
+  const endTimestamp = new Date(period.end).getTime();
+
+  return dateTimestamp >= startTimestamp && dateTimestamp <= endTimestamp;
 }
 
 /**
@@ -159,8 +171,25 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const dateObj = new Date(date);
+
+  const month = dateObj.getUTCMonth() + 1;
+  const day = dateObj.getUTCDate();
+  const year = dateObj.getUTCFullYear();
+
+  let hours = dateObj.getUTCHours();
+  const minutes = dateObj.getUTCMinutes();
+  const seconds = dateObj.getUTCSeconds();
+
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  hours %= 12;
+  hours = hours || 12;
+
+  const formattedMinutes = minutes.toString().padStart(2, '0');
+  const formattedSeconds = seconds.toString().padStart(2, '0');
+
+  return `${month}/${day}/${year}, ${hours}:${formattedMinutes}:${formattedSeconds} ${ampm}`;
 }
 
 /**
